4a21d931004d7630175f26dad99107300dda8809
marko1olo <barsukdana@gmail.com>
2026-06-21 00:07:35 +0400

    Perf: Use ultra-fast ujson instead of standard json to reduce CPU load during state saves

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ import faulthandler
 import gc
 import gzip
 import psutil
-import json
+import ujson as json
 import logging
 import os
 import tracemalloc
@@ -17151,7 +17151,7 @@ async def process_help_menu(callback: types.CallbackQuery, board_id: str | None,
 
 
 import io
-import json
+import ujson as json
 import re
 import time
 from wordcloud import WordCloud
