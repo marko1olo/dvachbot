744f57d3f6c776331c20409157482f7918d8a67f
marko1olo <96879250+marko1olo@users.noreply.github.com>
2026-06-26 22:09:01 +0000

    🔒 Fix command injection in run_cmd

--- pr_merger.py
+++ pr_merger.py
@@ -1,8 +1,11 @@
+import shlex
 import subprocess
 
 def run_cmd(cmd, check=True):
+    if isinstance(cmd, str):
+        cmd = shlex.split(cmd)
     try:
-        return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode('utf-8').strip()
+        return subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT).decode('utf-8').strip()
     except subprocess.CalledProcessError as e:
         if check:
             raise
